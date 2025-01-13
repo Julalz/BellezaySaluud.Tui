1e9c6ea5d8346623f36d35eeaeb93ec7c72eb309
Julian Work <julian@hotmail.com>
2025-01-13 12:50:14 +0100

    slider productos shared component

--- Bellezaysalud/src/componentes/pages/Servicios/Services.jsx
+++ Bellezaysalud/src/componentes/pages/Servicios/Services.jsx
@@ -1,4 +1,14 @@
+import React from "react";
 import Header from "../../header/Header";
+import ServicesCards from "../../shared/services/services-cards";
+
+import { Swiper, SwiperSlide } from "swiper/react"; 
+import "swiper/css"; 
+import "swiper/css/navigation"; 
+import "swiper/css/pagination"; 
+
+import nails from "../../../../public/icons/nails.svg"
+
 
 import "./services.css";
 
@@ -6,6 +16,25 @@ function Services() {
   return (
     <>
       <Header />
+      <div className="services-slider">
+        <Swiper
+          spaceBetween={20} 
+          slidesPerView={4} 
+          navigation={true} 
+          pagination={{ clickable: true }} 
+        >
+      
+          <SwiperSlide>
+              <ServicesCards cardImg={nails} cardTitle={"Prueba1"} cardText={"esto es una prueba"}/>
+          </SwiperSlide>
+          <SwiperSlide >
+            <ServicesCards cardTitle={"Prueba2"} cardText={"esto es una prueba"}/>
+          </SwiperSlide>
+          <SwiperSlide >
+            <ServicesCards cardTitle={"Prueba3"} cardText={"esto es una prueba"}/>
+          </SwiperSlide>
+            </Swiper>
+      </div>
     </>
   );
 }
---
+++ Bellezaysalud/src/componentes/shared/services/Services-cards.jsx
@@ -0,0 +1,17 @@
+import "./services-cards.css"
+
+function ServicesCards({cardTitle ,cardAlt, cardImg}) {
+    return (
+        <div>
+            <div className="card">
+                <div className="card-body">
+                    <img className="card-img-services" src={cardImg} alt="" />
+                    <h5 className="card-title">{cardTitle}</h5>
+                    {/* <a href="#" className="btn btn-primary">Reserva tu cita</a> */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ServicesCards
