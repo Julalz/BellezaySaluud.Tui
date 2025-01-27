18ef587d6b0cfad25bf934ee0a1a66a735fb8441
Julian Work <julian@hotmail.com>
2025-01-27 13:26:26 +0100

    structure of second section of home, belleza y salud born 30 year ago

--- Bellezaysalud/src/componentes/pages/Home/Home.jsx
+++ Bellezaysalud/src/componentes/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import img2 from "../../../../public/img/anniversary (Post de Instagram) (2).png
 import img3 from "../../../../public/img/anniversary (Post de Instagram) (1).png";
 
 import "./home.css";
+// const Link = "/conocenos"
 
 function Home() {
   return (
@@ -54,11 +55,13 @@ function Home() {
       </div>
       <section className="body-container2">
         <div className="who-is-mila-container">
+          <div>
           <img
             src="../../../../public/img/Mila.png"
             alt="Mila Belleza y Salud"
           ></img>
           <p className="text-vertical-mila">Mila Covelo</p>
+          </div>
           <div>
             <p>
               Da el primer paso hacia una experiencia de belleza única <br></br>
@@ -70,9 +73,23 @@ function Home() {
               expertos estudia tus necesidades para ofrecerte tratamientos a
               medida que brillen con tu estilo único y belleza natural
             </p>
+            <div className="horizontal-line"></div>
           </div>
         </div>
       </section>
+      <section className="body-container3">
+        <div>
+          <h2>Belleza y Salud nació hace 30 años con el objetivo de brindar bienestar y cuidado integral</h2>
+          <p>Acompañarte a lo largo de los años, para que vivas cada etapa de la vida sintiéndote mejor que nunca</p>
+          <img className="Higiene-facial" src="../../../../public/img/DSC02675.jpg" alt="Higiene facial"></img>
+          </div>
+        <div>
+        <img className="img-container3" src="../../../../public/img/DSC02748.png" alt="30 años de Belleza y Salud"></img>
+        <Button text={"Conocenos"} url={"/conocenos"}/>
+        </div>
+
+      </section>
+      <div className="horizontal-line"></div>
 
       <Footer />
     </body>
